099befbbe4d5b8592b5d7be75bf660f4be063635
AnarYeraz <119958696+AnarYeraz@users.noreply.github.com>
2022-12-06 22:12:21 +0500

    Сделал проект на гитхабе и сделал модель Tasktype

---
+++ src/Course/Tasktype.java
@@ -0,0 +1,12 @@
+package Course;
+
+public class Tasktype {
+    private final String string;
+    public Tasktype(String string){
+        this.string = string;
+    }
+
+    public String getString() {
+        return string;
+    }
+}
